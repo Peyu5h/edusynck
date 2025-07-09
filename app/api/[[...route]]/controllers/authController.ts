06ec6145284601506fbbc6cc159d80a84f1ca76d
Piyush <piyushvbagul916@gmail.com>
2025-07-09 22:15:29 +0530

    bcrypt-v2.2

--- app/api/[[...route]]/controllers/authController.ts
+++ app/api/[[...route]]/controllers/authController.ts
@@ -1,6 +1,6 @@
 import { Context } from "hono";
 import { prisma } from "~/lib/prisma";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import {
   registerSchema,
